badcd66d60470d647d1aec4300b15c99b8b3420a
faizankhan <faijankha7860@gmail.com>
2026-03-14 07:30:03 +0530

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,12 @@
+let hours = 0;
+let topics = 0;
+
+function addHour(){
+hours++;
+document.getElementById("hours").innerHTML = hours;
+}
+
+function addTopic(){
+topics++;
+document.getElementById("topics").innerHTML = topics;
+}
