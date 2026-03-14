6eb824319ba29206037f44c0b669d723607030c1
faizankhan <faijankha7860@gmail.com>
2026-03-14 15:37:00 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,21 +1,25 @@
 let time = 1500;
-let interval;
+let timer = null;
 
 function startTimer(){
 
-interval = setInterval(function(){
+if(timer !== null){
+return;
+}
 
-let minutes = Math.floor(time / 60);
-let seconds = time % 60;
+timer = setInterval(function(){
+
+let minutes = Math.floor(time/60);
+let seconds = time%60;
 
 document.getElementById("timer").innerHTML =
-minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+minutes + ":" + (seconds<10?"0":"") + seconds;
 
 time--;
 
 if(time < 0){
-clearInterval(interval);
-alert("Take a 5 minute break!");
+clearInterval(timer);
+alert("Time for break!");
 }
 
 },1000);
@@ -23,7 +27,31 @@ alert("Take a 5 minute break!");
 }
 
 function resetTimer(){
-clearInterval(interval);
+
+clearInterval(timer);
+
 time = 1500;
+
 document.getElementById("timer").innerHTML = "25:00";
+
+}
+
+/* Analytics chart */
+
+window.onload = function(){
+
+const ctx = document.getElementById("studyChart");
+
+new Chart(ctx,{
+type:"bar",
+data:{
+labels:["Mon","Tue","Wed","Thu","Fri"],
+datasets:[{
+label:"Study Hours",
+data:[2,3,4,2,5],
+backgroundColor:"blue"
+}]
+}
+});
+
 }
