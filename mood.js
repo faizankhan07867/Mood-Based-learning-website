b96d605c742f58d2ee3e22cb2ffb0cad5b3ca549
faizankhan <faijankha7860@gmail.com>
2026-03-14 15:35:47 +0530

    Update mood.js

--- mood.js
+++ mood.js
@@ -1,17 +1,33 @@
-function recommend(mood){
+function recommendMood(mood){
 
 let suggestion = document.getElementById("suggestion");
+let videoBox = document.getElementById("videoBox");
 
 if(mood === "tired"){
-suggestion.innerHTML = "Recommended: Watch a short 5 minute learning video.";
+
+suggestion.innerHTML = "You seem tired. Watch a relaxing learning video.";
+
+videoBox.innerHTML =
+'<iframe width="420" height="250" src="https://www.youtube.com/embed/2OEL4P1Rz04" allowfullscreen></iframe>';
+
 }
 
 else if(mood === "stressed"){
-suggestion.innerHTML = "Recommended: Try a quick interactive quiz.";
+
+suggestion.innerHTML = "Feeling stressed? Try this calming focus technique.";
+
+videoBox.innerHTML =
+'<iframe width="420" height="250" src="https://www.youtube.com/embed/inpok4MKVLM" allowfullscreen></iframe>';
+
 }
 
 else if(mood === "motivated"){
-suggestion.innerHTML = "Recommended: Start a full tutorial lesson.";
+
+suggestion.innerHTML = "Great! Start learning something powerful.";
+
+videoBox.innerHTML =
+'<iframe width="420" height="250" src="https://www.youtube.com/embed/rfscVS0vtbw" allowfullscreen></iframe>';
+
 }
 
 }
